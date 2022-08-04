209c7f91d5b30367f147ff89a32a2736d0d70063
unknown <jsirait@wellesley.edu>
2022-08-04 16:49:20 +0100

    add CORS

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask
 from routes import initialize_routes
 
+from flask_cors import CORS
+
 app = Flask(__name__)
+
+# used to sign cookies to protect against data tampering
+app.config['SECRET_KEY'] = "Hello from Flask ;)"
+
+# used to allow cross origin resource sharing e.g. requests from different hosts
+app.config['CORS_HEADERS'] = ['Content-Type', 'Authorization']
+CORS(app, support_credentials=True, resources={r"/api/*":{"origins":"*"}})
 
 
 initialize_routes(app)
